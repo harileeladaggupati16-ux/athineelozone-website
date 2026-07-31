9d3389c2e579c54588bbba796b420a65fe3e67f1
harileeladaggupati16-ux <harileeladaggupati16@gmail.com>
2026-07-31 16:59:49 +0530

    updated code

---
+++ JS/navbar.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     fetch("Components/navbar.html")
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error("Navbar not found");
+//             }
+//             return response.text();
+//         })
+//         .then(data => {
+//             document.getElementById("navbar").innerHTML = data;
+//         })
+//         .catch(error => console.error(error));
+// });
