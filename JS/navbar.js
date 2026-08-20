6819553a74233884f7cbfaddbe027aeb0ed0a9e3
harileeladaggupati16-ux <harileeladaggupati16@gmail.com>
2026-08-20 17:25:50 +0530

    updated code

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,13 +1,17 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     fetch("Components/navbar.html")
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error("Navbar not found");
-//             }
-//             return response.text();
-//         })
-//         .then(data => {
-//             document.getElementById("navbar").innerHTML = data;
-//         })
-//         .catch(error => console.error(error));
-// });
+document.addEventListener("DOMContentLoaded", function () {
+
+    fetch("/Components/navbar.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Cannot load navbar.html");
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+});
